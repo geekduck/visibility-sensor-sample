c7acab1668e7ebdb5ec9fd96fbb540c0174253a7
geekduck <duck1218+github@gmail.com>
2019-09-27 15:53:15 +0900

    CurrentTimerコンポーネントを実装した

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
 import './App.css';
+
+const CurrentTimer = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <div className="CurrentTimer">{dayjs(currentTime).format('YYYY/MM/DD HH:mm:ss.SSS')}</div>
+};
 
 function App() {
   return (
     <div className="App">
-      test
+      <hr className="up" />
+      <CurrentTimer/>
+      <hr className="down" />
     </div>
   );
 }
