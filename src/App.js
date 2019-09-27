90f94c69dcbde7c8f9227113c6e39dd33b677015
geekduck <duck1218+github@gmail.com>
2019-09-27 18:38:58 +0900

    表示領域に入っている場合だけ、タイマーを動かすようにした

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import dayjs from 'dayjs';
+import VisibilitySensor from 'react-visibility-sensor';
 import './App.css';
 
 const CurrentTimer = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
+    const [currentTime, setCurrentTime] = useState(new Date());
+    const [timerId, setTimerId] = useState(0);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1);
-    return () => clearTimeout(timer);
-  }, []);
+    useEffect(() => {
+        return () => clearInterval(timerId); // タイマーの後始末
+    }, [timerId]);
 
-  return <div className="CurrentTimer">{dayjs(currentTime).format('YYYY/MM/DD HH:mm:ss.SSS')}</div>
+    const onChange = (isVisible) => {
+        if (isVisible) {
+            // このコンポーネントが表示されている場合、タイマーで現在時刻を更新する。
+            setTimerId(setInterval(() => {
+                setCurrentTime(new Date());
+            }, 10));
+        } else {
+            // このコンポーネントが表示されていない場合、タイマーを削除して現在時刻を更新しない。
+            clearInterval(timerId);
+            setTimerId(undefined);
+        }
+    };
+
+    return (
+        <VisibilitySensor
+            onChange={onChange}
+            partialVisibility={true} // 表示領域にちょっとでも入っていたらisVisible=trueと判定させる。
+            offset={{top: 100, bottom: 100}} // 見た目でわかりやすいように、上下100pxの範囲は表示領域外にする。
+        >
+            <div className="CurrentTimer">{dayjs(currentTime).format('YYYY/MM/DD HH:mm:ss.SSS')}</div>
+        </VisibilitySensor>
+    );
 };
 
 function App() {
-  return (
-    <div className="App">
-      <hr className="up" />
-      <CurrentTimer/>
-      <hr className="down" />
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="upper-border"><span>表示領域外</span></div>
+            <CurrentTimer/>
+            <div className="lower-border"><span>表示領域外</span></div>
+        </div>
+    );
 }
 
 export default App;
